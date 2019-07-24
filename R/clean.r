3f7a4fc11afa4c99abaada28363cbb89f4d8c25b
Ben <ben.williams@alaska.gov>
2019-07-23 16:20:28 -0800

    updates

--- R/clean.r
+++ R/clean.r
@@ -6,75 +6,53 @@
 # 1. tows correspond to bed definitions
 f_tow_df <- function(events_data){
 
-  output_dir <- file.path("output", YEAR)
+    output_dir <- file.path("output", YEAR)
 
-  if (!dir.exists(output_dir)){
-    dir.create(output_dir)
+    if (!dir.exists(output_dir)){
+      dir.create(output_dir)
+    } else {
+      print("Good to go!")
+    }
+
+  if(YEAR < 2019){
+    events_data %>%
+      filter(# YEAR==YEAR,
+        GEAR_PERFORMANCE_CODE_SW == 1, # did the gear work
+        STATION_TYPE %in%
+          c("Standard", "Repeat", "Standard Non-Selected")) %>%
+      # multiple station types
+      transmute(tow_id = EVENT_ID,
+                Bed = factor(BED_SW),
+                area_swept = TOW_LENGTH_DESIGNATED * 0.00131663,
+                # in nautical miles
+                #area_swept = TOW_LENGTH_FIELD * 0.00131663,
+                # in nautical miles - switched for 2018 to make run before getting complete dataset from Mumm
+                station = STATION_ID) %>%
+      mutate(bedsta = paste0(tow_id, station)) -> x
   } else {
-    print("Good to go!")
+    events_data %>%
+      filter(gear_perf == 1,
+             haul_type == 10) %>%
+      transmute(tow_id = tow,
+                Bed = factor(bed_code),
+                area_swept = distance * 0.00131663, # in nautical miles
+                station = station) %>%
+      mutate(bedsta = paste0(tow_id, station)) -> x
   }
 
+    # make sure that there are no duplicate data
 
-  events_data %>%
-    filter(# YEAR==YEAR,
-      GEAR_PERFORMANCE_CODE_SW == 1, # did the gear work
-      STATION_TYPE %in%
-        c("Standard", "Repeat", "Standard Non-Selected")) %>%
-    # multiple station types
-    transmute(tow_id = EVENT_ID,
-              Bed = factor(BED_SW),
-              area_swept = TOW_LENGTH_DESIGNATED * 0.00131663,
-              # in nautical miles
-              #area_swept = TOW_LENGTH_FIELD * 0.00131663,
-              # in nautical miles - switched for 2018 to make run before getting complete dataset from Mumm
-              station = STATION_ID) %>%
-    mutate(bedsta = paste0(tow_id, station)) -> x
+    if(dim(x)[1] != length(unique(x$bedsta))) {
+      stop("Repeated station tows")
+    } else {
+      select(x, -station, - bedsta) -> x
+    }
 
-  # make sure that there are no duplicate data
-
-  if(dim(x)[1] != length(unique(x$bedsta))) {
-    stop("Repeated station tows")
-  } else {
-    select(x, -station, - bedsta) -> x
-  }
-  # save the output
-  write_csv(x, here::here(paste0("output/",YEAR,"/tows.csv")))
-  x
+    # save the output
+    write_csv(x, here::here(paste0("output/",YEAR,"/tows.csv")))
+    x
 }
 
-# the 2019 survey data is in a different format
-f_tow_df2 <- function(events_data){
-
-  output_dir <- file.path("output", YEAR)
-
-  if (!dir.exists(output_dir)){
-    dir.create(output_dir)
-  } else {
-    print("You made a folder - good job!")
-  }
-  # did the gear work
-  # multiple station types
-
-  events_data %>%
-    filter(gear_perf == 1,
-           haul_type == 10) %>%
-    transmute(tow_id = tow,
-              Bed = factor(bed_code),
-              area_swept = distance * 0.00131663, # in nautical miles
-              station = station) %>%
-    mutate(bedsta = paste0(tow_id, station)) -> x
-
-  # make sure that there are no duplicate data
-
-  if(dim(x) [1] != length(unique(x$bedsta))){
-    stop("Repeated station tows")
-  } else {
-    select(x, -station, -bedsta) -> x
-  }
-  # save the output
-  write_csv(x, here::here(paste0("output/",YEAR,"/tows.csv")))
-  x
-}
 
 # 2. beds (dependent upon the tow function)
 f_bed_df <- function(area_data, tows){
