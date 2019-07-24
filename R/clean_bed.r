531a36131b08de557e35ef4857fc4f21c445ef6b
Ben <ben.williams@alaska.gov>
2019-07-24 08:34:33 -0800

    clean bed

--- R/clean_bed.r
+++ R/clean_bed.r
@@ -1,10 +1,11 @@
 # 2. beds (dependent upon the tow function)
-clean_bed <- function(area_data, tows){
+clean_bed <- function(tows){
 
-  area_data %>%
-    dplyr::right_join(dplyr::count(tows, Bed)) %>%
-    dplyr::rename(tows = n) %>%
-    dplyr::mutate(Bed = factor(Bed)) -> x
+  area %>%
+    full_join(count(tows, Bed)) %>%
+    drop_na %>%
+    rename(tows = n) %>%
+    mutate(Bed = factor(Bed)) -> x
 
   write_csv(x, here::here(paste0("output/",YEAR,"/beds.csv")))
   x
